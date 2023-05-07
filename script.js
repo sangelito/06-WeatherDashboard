aa29c577d2a29511959681c043765bc9987b811d
sangelito <129316135+sangelito@users.noreply.github.com>
2023-05-07 15:58:24 -0500

    Error handler

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 var owmAPI = "12117e6db7383302b3fd7a3d3a922c37"; 
 var currentCity = ""; 
 var lastCity = ""; 
+
+// Error handler for fetch
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+
