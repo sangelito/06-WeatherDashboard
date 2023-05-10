ce4a258f6589915074df48285e6a71a2a9f47b71
sangelito <129316135+sangelito@users.noreply.github.com>
2023-05-10 14:07:10 -0500

    Weather won't append >:(

--- script.js
+++ script.js
@@ -33,7 +33,46 @@ var getCurrentConditions = (event) => {
 })
  }
 
-
+var getFiveDayForecast = (event) => {
+    let city = $('#search-city').val();
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    // Fetch from API
+    fetch(queryURL)
+        .then (handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let fiveDayForecastHTML = `
+            <h2>5-Day Forecast:</h2>
+            <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
+            for (let i = 0; i < response.list.length; i++) {
+                let dayData = response.list[i];
+                let dayTimeUTC = dayData.dt;
+                let timeZoneOffset = response.city.timezone;
+                let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+                let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+                let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+                // Only displaying mid-day forecasts
+                if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                    fiveDayForecastHTML += `
+                    <div class="weather-card card m-2 p0">
+                        <ul class="list-unstyled p-3">
+                            <li>${thisMoment.format("MM/DD/YY")}</li>
+                            <li class="weather-icon"><img src="${iconURL}"></li>
+                            <li>Temp: ${dayData.main.temp}&#8457;</li>
+                            <br>
+                            <li>Humidity: ${dayData.main.humidity}%</li>
+                        </ul>
+                    </div>`;
+                }
+            }
+            // Build the HTML template
+            fiveDayForecastHTML += `</div>`;
+            // Append the five-day forecast to the DOM
+            $('#five-day-forecast').html(fiveDayForecastHTML);
+        }) 
+}
  var saveCity = (newCity) => {
     let cityExists = false;
     // Check if City exists in local storage
@@ -71,4 +110,3 @@ var getCurrentConditions = (event) => {
 
 
 getCurrentConditions();
-
